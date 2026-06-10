f9d62ca36435833874a6429bfefd24ce7ab0f2f8
flymiyamoto <panchaosu12@gmail.com>
2026-06-10 22:22:22 +0200

    comments and votes

--- src/app/database-service.ts
+++ src/app/database-service.ts
@@ -4,17 +4,42 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class DatabaseService {
-  fauxPosts = [{
-    title:"",
-    src:"",
-    upvotes:"",
-    downvotes:"",
-    author:"",
-    
-
-
+  private fauxPosts = [{
+    id: 1,
+    title:"testTitle",
+    src:"/images/1.jpg",
+    upvotes: 24,
+    downvotes: 2,
+    comments: [
+      {
+        id: 1,
+        author:"Marcy",
+        content:"Ale gupi",
+        upvotes: 0,
+        downvotes: 200
+      }
+    ],
+    author:"Marcy",
   }]
 
+  private fauxProfile=[{
+    id:1,
+    username: "Marcy",
+    src:"/images/marcy.png",
+    comments:[
+      {
+        id: 1,
+        author:"Marcy",
+        content:"Ale gupia",
+        upvotes: 0,
+        downvotes: 200
+      }
+    ]
+  }]
+
+  async getPostsHome(){
+    return this.fauxPosts
+  }
 
 
 }
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,136 @@
-import { Component } from '@angular/core';
+import { Component, inject, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { DatabaseService } from '../database-service';
+import { arrayBuffer } from 'stream/consumers';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Home {}
+export class Home {
+  data= inject(DatabaseService)
+  cdr = inject(ChangeDetectorRef)
+  postsLoaded= false
+  posts = Array<any>()
+  userVotes = new Map<number, 'upvote' | 'downvote' | null>()
+  expandedComments = new Set<number>()
+  userCommentVotes: { [key: string]: 'upvote' | 'downvote' | null } = {}
+
+  async ngOnInit() {
+    try{
+      this.posts = await this.data.getPostsHome()
+      this.posts.forEach(post => {
+        this.userVotes.set(post.id, null)
+      })
+      this.postsLoaded = true
+      this.cdr.markForCheck()
+    }
+    catch(e){
+
+    }
+
+
+  }
+
+  upvotePost(id : number){
+    const post = this.posts.find(p => p.id === id)
+    if (!post) return
+
+    const currentVote = this.userVotes.get(id)
+    
+    if (currentVote === 'upvote') {
+      post.upvotes = Number(post.upvotes) - 1
+      this.userVotes.set(id, null)
+    } else {
+      if (currentVote === 'downvote') {
+        post.downvotes = Number(post.downvotes) - 1
+      }
+      post.upvotes = Number(post.upvotes) + 1
+      this.userVotes.set(id, 'upvote')
+    }
+    this.cdr.markForCheck()
+  }
+
+  downvotePost(id:number){
+    const post = this.posts.find(p => p.id === id)
+    if (!post) return
+
+    const currentVote = this.userVotes.get(id)
+    
+    if (currentVote === 'downvote') {
+      post.downvotes = Number(post.downvotes) - 1
+      this.userVotes.set(id, null)
+    } else {
+      if (currentVote === 'upvote') {
+        post.upvotes = Number(post.upvotes) - 1
+      }
+      post.downvotes = Number(post.downvotes) + 1
+      this.userVotes.set(id, 'downvote')
+    }
+    this.cdr.markForCheck()
+  }
+
+
+  toggleComments(postId: number) {
+    if (this.expandedComments.has(postId)) {
+      this.expandedComments.delete(postId)
+    } else {
+      this.expandedComments.add(postId)
+    }
+    this.cdr.markForCheck()
+  }
+
+  getCommentVoteKey(postId: number, commentId: number): string {
+    return `${postId}-${commentId}`
+  }
+
+  upvoteComment(postId: number, commentId: number) {
+    const post = this.posts.find((p: any) => p.id === postId)
+    if (!post || !post.comments) return
+
+    const comment = post.comments.find((c: any) => c.id === commentId)
+    if (!comment) return
+
+    const voteKey = this.getCommentVoteKey(postId, commentId)
+    const currentVote = this.userCommentVotes[voteKey]
+
+    if (currentVote === 'upvote') {
+      comment.upvotes = (Number(comment.upvotes) || 0) - 1
+      this.userCommentVotes[voteKey] = null
+    } else {
+      if (currentVote === 'downvote') {
+        comment.downvotes = (Number(comment.downvotes) || 0) - 1
+      }
+      comment.upvotes = (Number(comment.upvotes) || 0) + 1
+      this.userCommentVotes[voteKey] = 'upvote'
+    }
+    this.cdr.markForCheck()
+  }
+
+  downvoteComment(postId: number, commentId: number) {
+    const post = this.posts.find((p: any) => p.id === postId)
+    if (!post || !post.comments) return
+
+    const comment = post.comments.find((c: any) => c.id === commentId)
+    if (!comment) return
+
+    const voteKey = this.getCommentVoteKey(postId, commentId)
+    const currentVote = this.userCommentVotes[voteKey]
+
+    if (currentVote === 'downvote') {
+      comment.downvotes = (Number(comment.downvotes) || 0) - 1
+      this.userCommentVotes[voteKey] = null
+    } else {
+      if (currentVote === 'upvote') {
+        comment.upvotes = (Number(comment.upvotes) || 0) - 1
+      }
+      comment.downvotes = (Number(comment.downvotes) || 0) + 1
+      this.userCommentVotes[voteKey] = 'downvote'
+    }
+    this.cdr.markForCheck()
+  }
+
+
+}
